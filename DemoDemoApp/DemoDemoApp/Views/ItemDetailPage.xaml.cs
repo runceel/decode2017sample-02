5df03bed4ca17842743c37ada87d90efde8ff31c
Kazuki Ota <kaota@microsoft.com>
2017-04-05 15:58:33 +0900

    プロジェクト ファイルを追加します。

---
+++ DemoDemoApp/DemoDemoApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using DemoDemoApp.ViewModels;
+
+using Xamarin.Forms;
+
+namespace DemoDemoApp.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
